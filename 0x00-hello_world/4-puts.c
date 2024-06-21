1cd926d2fc6659055023f2aed705573bec93ef40
carolyneMuthe <mutheuc08@gmail.com>
2024-06-21 08:53:47 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
+#include <stdio.h>
+
 /**
  * main - Entry point of the program
  *
- * Description: Prints a specific phrase using printf
+ * Description:
+ *     Prints the phrase "Programming is like building a multilingual puzzle"
+ *     using the puts function.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("/"Programming is like building a multilingual puzzle");
-	return (0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
+
