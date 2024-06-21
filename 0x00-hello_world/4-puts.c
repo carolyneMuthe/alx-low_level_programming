64dc030f505e702714c89db4e257f34e47fc2791
carolyneMuthe <mutheuc08@gmail.com>
2024-06-21 06:59:49 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a specific phrase using printf
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Programming is like building a multilingual puzzle, /n");
+	return (0);
+}
