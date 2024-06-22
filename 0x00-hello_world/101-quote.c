e215912c27c45e96e3ea4cb8b3ae0299761657bd
carolyneMuthe <mutheuc08@gmail.com>
2024-06-22 05:50:22 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; 
+	const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(2, msg, 59);
-	return (1);
+	return (1)
 }
