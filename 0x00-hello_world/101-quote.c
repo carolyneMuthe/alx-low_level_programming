8ae66cd3ea3ec082b40adf9590d339f0a4185974
carolyneMuthe <mutheuc08@gmail.com>
2024-06-22 05:33:03 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
+/*
+ *  main - Entry point
+ *
+ * Description: Prints a specific string to the standard error.
+ * Return: Always 1 (indicating an error for the purpose of the task)
+ */
 int main(void)
 {
-	const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char *msg ="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	 write(2, msg, 59);
 	 return (1);
 }
