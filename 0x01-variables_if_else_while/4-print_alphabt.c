866dd235b3d17e1b10527e719f5364480da2d2e3
carolyneMuthe <mutheuc08@gmail.com>
2024-06-24 14:02:57 -0700

    Print all the letters except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,16 +7,18 @@
  */
 int main(void)
 {
-	char lowercase = 'a';
+    char lowercase = 'a';
 
-	while (lowercase <= 'z')
-	{
-		if (lowercase != 'e' && lowercase != 'q')
-		{
-	putchar(lowercase);
-		}
-		lowercase++;
-	}
-	putchar('\n');
-	return (0);
+    while (lowercase <= 'z')
+    {
+        if (lowercase != 'e' && lowercase != 'q')
+        {
+            putchar(lowercase);
+        }
+        lowercase++;
+    }
+
+    putchar('\n');  // Print newline character
+
+    return 0;
 }
