34aae402b33c258ee0e4ad98ca04d2499340fcbf
carolyneMuthe <mutheuc08@gmail.com>
2024-06-24 16:10:31 -0700

    A program that prints all possible different combinations of three digits.

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints all possible different combinations
+ *              of three digits in ascending order, separated by ", ".
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int hundreds_digit, tens_digit, ones_digit;
+
+    for (hundreds_digit = 0; hundreds_digit <= 7; hundreds_digit++)
+    {
+        for (tens_digit = hundreds_digit + 1; tens_digit <= 8; tens_digit++)
+        {
+            for (ones_digit = tens_digit + 1; ones_digit <= 9; ones_digit++)
+            {
+                putchar(hundreds_digit + '0');   // Print hundreds digit
+                putchar(tens_digit + '0');       // Print tens digit
+                putchar(ones_digit + '0');       // Print ones digit
+
+                if (hundreds_digit != 7 || tens_digit != 8 || ones_digit != 9)
+                {
+                    putchar(',');   // Print comma
+                    putchar(' ');   // Print space
+                }
+            }
+        }
+    }
+
+    putchar('\n');  // Print newline character
+
+    return 0;
+}
+
