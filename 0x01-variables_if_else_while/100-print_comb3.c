1e1bd37e35dec2b312a2eee91bee3514225fdf07
carolyneMuthe <mutheuc08@gmail.com>
2024-06-24 16:14:14 -0700

    A program that prints all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,29 +1,33 @@
 #include <stdio.h>
+
 /**
  * main - Entry point of the program
  *
- * Description: This program prints all possible different combinations
- *              of two digits in ascending order, separated by ", ".
+ * Description: This program prints all possible combinations of two digits
+ *              separated by ", ", with the smallest combination first.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	 int tens_digit, ones_digit;
+	int tens_digit, ones_digit;
 
-	 for tens_digit = 0; tens_digit <=8; tens_digit++)
-	 {
-		 for (ones_digit = tens_digit + 1; ones_digit <=9; ones_digit++)
-		 {
-			 putchar(tens_digit + '0');
-			 putchar(ones_digit + '0');
-			 if (tens_digit != 8 || ones_digit != 9)
-			 {
-				 putchar(',');
-				 putchar(' ');
-			 }
-		 }
-	 }
-	 putchar('\n')
-		 return (0);
+	for (tens_digit = 0; tens_digit <= 8; tens_digit++)
+	{
+		for (ones_digit = tens_digit + 1; ones_digit <= 9; ones_digit++)
+		{
+			putchar(tens_digit + '0');   /* Print tens digit */
+			putchar(ones_digit + '0');   /* Print ones digit */
+
+			if (tens_digit != 8 || ones_digit != 9)
+			{
+				putchar(',');   /* Print comma */
+				putchar(' ');   /* Print space */
+			}
+		}
+	}
+
+	putchar('\n');  /* Print newline character */
+
+	return (0);
 }
