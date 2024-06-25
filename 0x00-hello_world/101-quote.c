2a1fddaf7268821ec21ae9883c639c097c73c71d
carolyneMuthe <mutheuc08@gmail.com>
2024-06-24 17:49:52 -0700

    A C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,25 @@
 #include <unistd.h>
+#include <stdlib.h>
 
 /**
- * main - Entry point
+ * main - Entry point of the program
  *
- * Description: Prints a specific string to the standard error.
- * Return: Always 1 (indicating an error for the purpose of the task)
+ * Return: Always 1 (indicating an error)
  */
 int main(void)
 {
-    const char msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    ssize_t len = sizeof(msg) - 1; /* Calculate message length */
+    /* Message to be printed */
+    char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-    /* Use write system call to output to stderr (file descriptor 2) */
-    write(2, msg, len);
+    /* Calculate the length of the message */
+    int length = 0;
+    while (message[length] != '\0')
+        length++;
 
-    return (1); /* Return 1 to indicate an error */
+    /* Write the message to standard error using write system call */
+    if (write(STDERR_FILENO, message, length) != length)
+        return (EXIT_FAILURE); /* Return failure if write fails */
+
+    return 1; /* Return 1 as specified */
 }
+
