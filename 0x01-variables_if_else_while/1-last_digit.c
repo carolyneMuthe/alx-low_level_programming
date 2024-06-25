e72a91e409b3cb8ebb135baebabdf04f583d3090
carolyneMuthe <mutheuc08@gmail.com>
2024-06-24 17:02:11 -0700

    A  program prints the alphabet in lowercase

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - Entry point of the program
  *
- * Description: This program assigns a random number to the variable n each time it is executed.
+ * Description: This program assigns a random number to the variable n each
+ * time it is executed. It then prints the last digit of the number stored in
+ * n and states whether it is greater than 5, is 0, or is less than 6 and not 0.
  *
  * Return: Always 0 (Success)
  */
