d7debc462642f494c05e807e58ff5aa137ae8238
carolyneMuthe <mutheuc08@gmail.com>
2024-06-24 17:46:00 -0700

    A C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,11 +8,11 @@
  */
 int main(void)
 {
-	const char msg[] ="and that piece of art is useful\" - Dora Korpar,2015-10-19\n";
-	ssize_t len = sizeof(msg) - 1;
+    const char msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    ssize_t len = sizeof(msg) - 1; /* Calculate message length */
 
     /* Use write system call to output to stderr (file descriptor 2) */
-	write(2, msg, len);
+    write(2, msg, len);
 
-	return (1); /* Return 1 to indicate an error */
+    return (1); /* Return 1 to indicate an error */
 }
