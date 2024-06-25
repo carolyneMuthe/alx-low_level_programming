8a80b6bcd58fb12efc6f7da81542acefb94dd052
carolyneMuthe <mutheuc08@gmail.com>
2024-06-24 18:33:28 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,22 +1,24 @@
-#include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 
 /**
- * main - Entry point
- * Return: Always 1 (Success)
+ * main - Entry point of the program
+ *
+ * Return: Always 1 (indicating an error)
  */
 int main(void)
 {
-	char *msg = "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
-
+	char *message = "and that piece of art is useful\" - "
+			"Dora Korpar, 2015-10-19\n";
 	int length = 0;
-	while (sg[length] != '\0)
+
+	while (message[length] != '\0')
 		length++;
+
 	if (write(STDERR_FILENO, message, length) != length)
 	{
-		return(EXIT_FAILURE);
+		return (EXIT_FAILURE);
 	}
+
 	return (1);
 }
