be9f574f8c914fe201b8fa11dc23eeb320dce1ea
carolyneMuthe <mutheuc08@gmail.com>
2024-06-29 15:38:10 -0700

    Fix Betty style issues and update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 
 /**
- * main - Prints the first 98 Fibonacci numbers, starting with 1 and 2.
+ * main - prints the first 98 Fibonacci numbers
+ * starting with 1 and 2, followed by a new line.
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	unsigned long a = 1, b = 2, c;
-	int count;
+	unsigned long int a = 1, b = 2, c;
+	int i;
 
-	/* Print the first two Fibonacci numbers */
 	printf("%lu, %lu", a, b);
 
-	/* Loop to calculate and print the next 96 Fibonacci numbers */
-	for (count = 3; count <= 98; count++)
+	for (i = 3; i <= 98; i++)
 	{
 		c = a + b;
 		printf(", %lu", c);
-
 		a = b;
 		b = c;
 	}
+
 	printf("\n");
+
 	return (0);
 }
