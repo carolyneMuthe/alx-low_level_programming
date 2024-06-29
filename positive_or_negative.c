34b962cd9ebe7d2ba045ba556533c976c939641d
carolyneMuthe <mutheuc08@gmail.com>
2024-06-29 16:28:19 -0700

    Multiple mains

--- positive_or_negative.c
+++ positive_or_negative.c
@@ -1,22 +1,15 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * positive_or_negative - prints if integer is positive, negative, or zero
- * @i: integer to be checked
+ * positive_or_negative - prints if the number is positive, negative or zero
+ * @i: the integer to check
  */
 void positive_or_negative(int i)
 {
     if (i > 0)
-    {
         printf("%d is positive\n", i);
-    }
     else if (i < 0)
-    {
         printf("%d is negative\n", i);
-    }
     else
-    {
         printf("%d is zero\n", i);
-    }
 }
