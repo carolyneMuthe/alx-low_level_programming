fb48e3bab798a63928b281370a249a2c2785a412
carolyneMuthe <mutheuc08@gmail.com>
2024-07-01 11:02:10 -0700

    Write a function that returns the length of a string.

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,4 +4,5 @@
 #include <stdio.h> 
 void reset_to_98(int *n);/* updates the value it points to to 98. */
 void swap_int(int *a, int *b); /* swaps the values of two integers. */
+int _strlen(char *s); /* returns the length of a string. */
 #endif /* MAIN_H */
