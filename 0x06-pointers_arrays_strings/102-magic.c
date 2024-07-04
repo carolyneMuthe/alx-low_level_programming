37e5149625208ebd409cbefb05b03205f412d2fe
carolyneMuthe <mutheuc08@gmail.com>
2024-07-04 12:56:33 -0700

    Add one line to this code, so that the program prints a[2] = 98, followed by a new line.
    You are not allowed to use the variable a in your new line of code
    You are not allowed to modify the variable p
    You can only write one statement
    You are not allowed to use ,
    You are not allowed to code anything else than the line of expected line of code at the expected line
    Your code should be written at line 19, before the ;
    Do not remove anything from the initial code (not even the comments)
    You are allowed to use the standard library

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
-/**
- * main - Entry point
- *
- * Description: This program demonstrates pointer arithmetic to change
- *              the value of an array element without directly accessing it.
- *
- * Return: Always 0 (Success)
- */
+
 int main(void)
 {
-	int n;
-	int a[5];
-	int *p;
+  int n;
+  int a[5];
+  int *p;
 
-	a[2] = 1024;
-	p = &n;
-/*
+  a[2] = 1024;
+  p = &n;
+  /*
    * write your line of code here...
    * Remember:
    * - you are not allowed to use a
@@ -23,8 +16,8 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-	*(p + 5) = 98;
-
-	printf("a[2] = %d\n", a[2]);
-	return (0);
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
